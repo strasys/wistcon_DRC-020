c590bfbcc7627dbabd6b65952a18263234b35d30
Johannes <johannes.strasser@strasys.at>
2017-12-31 12:10:14 +0100

    css and iniphp

--- webinterface/privateplc_php.ini.php
+++ webinterface/privateplc_php.ini.php
@@ -3,4 +3,5 @@ ini_set('session.name', 'privateplc_login');
 ini_set('session.use_trans_sid', '0');
 ini_set('session.use_cookies' , '1' );
 ini_set('session.use_only_cookies' , '1');
+ini_set('date.timezone', 'Europe/Berlin');
 ?>
